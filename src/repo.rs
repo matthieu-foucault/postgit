7fe295a3d2c25de97c2aa67987fb19b361c475bf
Matthieu Foucault <foucault.matthieu@gmail.com>
2022-12-02 16:44:22 -0800

    first attempt at merging files using a graph

--- src/repo.rs
+++ src/repo.rs
@@ -1,10 +1,13 @@
-use std::path::Path;
-
 use anyhow::{bail, Result};
 use git_repository::bstr::ByteSlice;
 use git_repository::objs::tree::EntryMode;
 use git_repository::traverse::tree::Recorder;
 use git_repository::{Commit, Repository};
+use petgraph::algo::toposort;
+use petgraph::prelude::{DiGraph, DiGraphMap, UnGraph};
+use regex::Regex;
+use std::collections::HashMap;
+use std::path::Path;
 
 pub fn get_schema_script(repo_path: &str, ref_or_sha1: &str, schema_path: &str) -> Result<String> {
     let repo_path = Path::new(repo_path);
@@ -56,4 +59,77 @@ fn try_find_commit<'repo>(
     } else {
         Ok(None)
     }
+}
+
+fn merge_sql_scripts(sql_scripts: &HashMap<&str, &str>) -> Result<String> {
+    let import_regex = Regex::new(r"(m?)^.*--\s*import\s+(.*)$").unwrap();
+    let mut edges: Vec<(String, String)> = vec![];
+    for (k, v) in sql_scripts.iter() {
+        let mut parent = "".to_string();
+        for path_part in k.split('/') {
+            let mut full_file_path = path_part.to_string();
+            if !parent.is_empty() {
+                full_file_path = parent.clone() + "/" + path_part;
+                edges.push((parent.clone(), full_file_path.clone()));
+            }
+            parent = full_file_path;
+        }
+
+        for group in import_regex.captures_iter(v) {
+            edges.push((group[0].to_string(), k.to_string()));
+        }
+    }
+
+    let str_edges: Vec<(&str, &str)> = edges
+        .iter()
+        .map(|&(ref x, ref y)| (x.as_str(), y.as_str()))
+        .collect();
+
+    let graph = DiGraphMap::<_, ()>::from_edges(str_edges);
+    let sorted_nodes = toposort(&graph, None);
+    match sorted_nodes {
+        Ok(nodes) => Ok(nodes
+            .iter()
+            .map(|key| sql_scripts.get(key))
+            .filter_map(|e| e)
+            .map(|e| *e)
+            .collect::<Vec<&str>>()
+            .join("\n")),
+        Err(_) => bail!("Dependency cycle found."),
+    }
+}
+
+#[test]
+fn it_merges_sql_scripts_in_order() {
+    let mut scripts = HashMap::new();
+    scripts.insert(
+        "schema/a",
+        r#"
+    -- import schema/b
+
+    create table foo.bar(
+        id int primary key
+    );
+    "#,
+    );
+    scripts.insert(
+        "schema/b",
+        r#"
+    create schema foo;
+    "#,
+    );
+
+    let merged_script = merge_sql_scripts(&scripts);
+    assert_eq!(
+        r#"
+    create schema foo;
+    -- import schema/b
+
+    create table foo.bar(
+        id int primary key
+    );
+    "#
+        .to_string(),
+        merged_script.unwrap()
+    );
 }
